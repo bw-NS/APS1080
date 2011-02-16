9279dfeb45592e039ece6d2f98ff06ee1e55fd4a
Bowen Wu <bowen806@hotmail.com>
2011-02-16 14:00:00 +0100

    reset to 2011

---
+++ Tic-tac-toe/README.md
@@ -0,0 +1,2 @@
+1.maintains TTT borad -- clear, set , terminate
+2.computer player -- 1.win 2.not lose
---
+++ Tic-tac-toe/Tic-tac-toe.py
@@ -0,0 +1,8 @@
+class Tic_Tac_Toe:
+    def __init__(self) -> None:
+        
+    def clear:
+    
+    def terminate:
+
+    def set_position:
